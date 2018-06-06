2b7e78139f2a62e0dbf7644bc9a2a447f2795f38
baileyshelden <bailey7748@gmail.com>
2018-06-06 20:17:22 +0000

    automatically backed up by learn

--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,10 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
-
+def mothers_day(mom)
+  puts "Happy mother's day, #{mom}!"
+end
 
 ## Call your method below - use puts to see it in the terminal!
 
+mothers_day("Beyonce")
+mothers_day("Mom")
